a4b72a19bb6b934c893aef4e383c3199feca15f7
Antonio Ortiz <ortiz.antonio@gmail.com>
2024-10-17 15:02:16 -0500

    formatted with prettier

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,17 +1,17 @@
-import { EleventyRenderPlugin } from "@11ty/eleventy";
-import Nunjucks from "nunjucks";
+import { EleventyRenderPlugin } from '@11ty/eleventy'
+import Nunjucks from 'nunjucks'
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPlugin(EleventyRenderPlugin)
   let nunjucksEnvironment = new Nunjucks.Environment(
-    new Nunjucks.FileSystemLoader("_includes/src/partials"),
-  );
+    new Nunjucks.FileSystemLoader('_includes/src/partials')
+  )
 
-  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
-  eleventyConfig.addPassthroughCopy({ assets: "assets" });
+  eleventyConfig.setLibrary('njk', nunjucksEnvironment)
+  eleventyConfig.addPassthroughCopy({ assets: 'assets' })
   return {
     dir: {
-      output: "public",
+      output: 'public',
     },
-  };
+  }
 }
